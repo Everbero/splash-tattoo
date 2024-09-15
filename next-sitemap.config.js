6885bd9571cc2ccd17dd5367beb845e59e119635
Douglas Everbero <dougas@3xweb.site>
2024-09-15 14:15:23 -0300

    chore: fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,1 +1,9 @@
-s
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.splashtattoo.com.br', // URL do seu site
+    generateRobotsTxt: true, // Gera também um arquivo robots.txt
+    sitemapSize: 5000, // Número máximo de URLs por sitemap
+    changefreq: 'daily', // Frequência de mudança das páginas
+    priority: 0.7, // Prioridade padrão para as páginas
+  };
+  
