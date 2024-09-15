b81d251f8aef4363642050edb5b881cf85c3d7ed
Douglas Everbero <dougas@3xweb.site>
2024-09-15 14:23:52 -0300

    upgrading sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,22 @@
+const data = require('./app/lib/db.json'); // Importando o arquivo de dados do portfólio
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.splashtattoo.com.br', // URL do seu site
-    generateRobotsTxt: true, // Gera também um arquivo robots.txt
-    sitemapSize: 5000, // Número máximo de URLs por sitemap
-    changefreq: 'daily', // Frequência de mudança das páginas
-    priority: 0.7, // Prioridade padrão para as páginas
-  };
-  
+  siteUrl: 'https://www.splashtattoo.com.br',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+
+  // Função para gerar paths adicionais (rotas dinâmicas) para o sitemap
+  additionalPaths: async (config) => {
+    // Gerando as rotas para cada item do portfólio com base no db.json
+    const portfolioPaths = data.portfolio.map((item) => ({
+      loc: `/portfolio/${item.artist.toLowerCase().replace(/ /g, '-')}/${item.id}`, // Gerando a URL de cada item
+      changefreq: 'monthly',
+      priority: 0.8, // Você pode ajustar a prioridade individual de cada página
+    }));
+
+    return portfolioPaths;
+  },
+};
